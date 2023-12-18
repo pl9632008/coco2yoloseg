c8b4e0923cf6ea924f04642cf1a750846d5510ee
pl9632008 <67965957+pl9632008@users.noreply.github.com>
2023-12-18 17:46:27 +0800

    Add files via upload

---
+++ voc2yolo/config.py
@@ -0,0 +1,20 @@
+"""Configurations
+File paths to labels and image/xml files
+"""
+LABEL_DIR = r"G:\Edge_downloads\VisDrone\VisDrone2019-DET-test-dev\test-dev_labels"
+IMAGE_DIR = r"G:\Edge_downloads\VisDrone\VisDrone2019-DET-test-dev\images"
+XML_DIR = r"G:\Edge_downloads\VisDrone\VisDrone2019-DET-test-dev\Annotations_yolo_converted_for_voc"
+
+names = [
+    "rail",
+    "person",
+    "car",
+    "truck",
+    "bus",
+    "train",
+    "bicycle",
+    "motorcycle",
+    "traffic light",
+    "tricycle",
+    "awning-tricycle"
+]
